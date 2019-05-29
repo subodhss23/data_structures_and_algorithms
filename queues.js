dc0f0a458a5fe626ee10504299b5871efef9f44b
Subodh Sharma <subodhsharma@subodh.local>
2019-05-28 18:50:25 -0700

    queues

--- queues.js
+++ queues.js
@@ -11,12 +11,27 @@ function createQueue(){
 
        remove (){
            return queue.pop()
-       }
+       },
 
-        // add or enqueue
-        // remove or dequeue
-        // peek
-        // length
-        // isEmpty
+       peek(){
+           return queue[queue.length - 1];
+       },
+       
+       get length(){
+           return queue.length;
+       },
+
+       isEmpty(){
+           return queue.length === 0;
+       }
     }
 }
+
+const q = createQueue();
+q.add('Make an egghead lesson');
+q.add('this is how I am learning data structure.');
+q.add('In this month I will master data strucure.')
+
+q.remove();
+q.remove();
+console.log(q.peek());
