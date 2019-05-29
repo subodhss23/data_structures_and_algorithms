9c595c13de4291346ccb40a3335d7d121bf90a3c
Subodh Sharma <subodhss23@gmail.com>
2019-05-29 03:14:26 -0700

    insertion sort

--- insertion_sort.js
+++ insertion_sort.js
@@ -3,7 +3,7 @@ var array = [9,1,2,5,6,3,4,8,7]
 
 function insertionSort(array){
     for (var i = 0; i < array.length; i ++){
-        var temp = arrar[i];
+        var temp = array[i];
         var j = i - 1;
         while (j >= 0 && array[j] > temp){
             array[j + 1] = array[j];
@@ -14,4 +14,4 @@ function insertionSort(array){
     return array;
 }
 
-console.elog(insertionSort(array));
+console.log(insertionSort(array));
