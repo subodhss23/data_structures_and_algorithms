65aedcce608acf8d641a240d1134937f2af648cf
Subodh Sharma <subodhss23@gmail.com>
2019-05-28 23:24:25 -0700

    linkedlist

--- linkedList.js
+++ linkedList.js
@@ -5,4 +5,34 @@ function createNode(value){
     }
 }
 
-function
+function createLinkedList(){
+    return {
+        head = null,
+        tail = null,
+        length = 0,
+
+        //push
+        push(value){
+            const node = createNode(value);
+
+            if (this.head === null){
+                this.head = node;
+                this.tail = node;
+                this.length++;
+                return node;
+            }
+
+            this.tail.next = node;
+            this.tail = node;
+            this.length++;
+
+            return node;
+        },
+        //pop
+        //get
+        //delete
+        isEmpty(){
+            return this.length === 0;
+        }
+    }
+}
