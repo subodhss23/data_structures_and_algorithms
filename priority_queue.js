456c4be8853418d41c31bffc5fa585f5f9b00ca0
Subodh Sharma <subodhss23@gmail.com>
2019-05-30 02:27:34 -0700

    priority done

--- priority_queue.js
+++ priority_queue.js
@@ -12,7 +12,7 @@ function PriorityQueue(){
         } else {
             var added = false;
             for (var i = 0; i < collection.length; i++){
-                if (element[i] < collection[i][1]){         //checking priorities
+                if (element[1] < collection[i][1]){         //checking priorities
                     collection.splice(i, 0, element);
                     added = true;
                     break;
@@ -27,5 +27,26 @@ function PriorityQueue(){
     this.dequeue = function() {
         var value = collection.shift();
         return value[0];
-    }
+    };
+
+    this.front = function(){
+        return collection[0];
+    };
+
+    this.size = function(){
+        return collection.length;
+    };
+
+    this.isEmpty = function(){
+        return (collection.length === 0)
+    };
 }
+
+var pq = new PriorityQueue();
+pq.enqueue(['Russle Peter', 2]);
+pq.enqueue(['Will Smith', 3]);
+pq.enqueue(['Rick Ross', 1]);
+pq.printCollection();
+pq.dequeue();
+pq.front();
+pq.printCollection();
