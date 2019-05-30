c2623a971fbfd66e58a6e7d0a206d26530e6f5cc
Subodh Sharma <subodhss23@gmail.com>
2019-05-30 03:07:34 -0700

    adding isPresent and remove methods

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -34,6 +34,69 @@ class BST {
                     } else if(node.right !== null){
                         return searchTree(node.right);
                     }
+                } ekse {
+                    return null;
+                }
+            };
+            return searchTree(node);
+        }
+    }
+
+    findMin(){
+        let current = this.root;
+        while (current.left !== null){
+            current = current.left;
+        }
+        return current.data;
+    }
+
+    findMax(){
+        let current = this.root;
+        while (current.right !== null){
+            current = current.right;
+        }
+        return current.data;
+    }
+
+    find(data){
+        let current = this.root;
+        while (current.data !== data){
+            if (data < current.data){
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+            if (current === null){
+                return null;
+            }
+        }
+        return current;
+    }
+
+    isPresent(data){
+        let current = this.root;
+        while (current){
+            if (data === current.data){
+                return true;
+            }
+            if (data < current.data){
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return false;
+    }
+
+    remove(data){
+        const removeNode = function(node, data){
+            if (node == null){
+                return null;
+            }
+            if (data == node.data){
+                //node has no children
+                if (node.left == null && node.right == null){
+                    return null;
                 }
             }
         }
